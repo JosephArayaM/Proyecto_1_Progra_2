cda787554e4970e19179c8d5e03d8f5e678ec082
chrisb2004 <chrisblanco.2004@gmail.com>
2025-09-21 10:50:19 -0600

    Update InterfazPersona.cpp

--- InterfazPersona.cpp
+++ InterfazPersona.cpp
@@ -4,13 +4,19 @@
 
 #include "InterfazPersona.h"
 
-InterfazPersona::InterfazPersona(const string& nombre, bool indicePrioridad)
-            : nombre(nombre), indicePrioridad(indicePrioridad) {}
+InterfazPersona::InterfazPersona(const string& nombre, const string& tipoPersona, int edad, bool indicePrioridad)
+            : nombre(nombre), tipoPersona(tipoPersona), edad(edad), indicePrioridad(indicePrioridad) {}
 
 InterfazPersona::~InterfazPersona() = default;
 
 [[nodiscard]] string InterfazPersona::getNombre() const { return nombre; }
 void InterfazPersona::setNombre(const string& newNombre) { this->nombre = newNombre; }
+
+[[nodiscard]] string InterfazPersona::getTipoPersona() const { return tipoPersona; }
+void InterfazPersona::setTipoPersona(const string& newTipo) { this->tipoPersona = newTipo; }
+
+[[nodiscard]] int InterfazPersona::getEdad() const { return edad; }
+void InterfazPersona::setEdad(int newEdad) { this->edad = newEdad; }
 
 [[nodiscard]] bool InterfazPersona::getIndicePrioridad() const { return indicePrioridad; }
 void InterfazPersona::setIndicePrioridad(bool newIndicePrioridad) { this->indicePrioridad = newIndicePrioridad; }
@@ -18,11 +24,15 @@ void InterfazPersona::setIndicePrioridad(bool newIndicePrioridad) { this->indice
 Json::Value InterfazPersona::toJson() const {
     Json::Value obj;
     obj["nombre"] = nombre;
+    obj["tipoPersona"] = tipoPersona;
+    obj["edad"] = edad;
     obj["indicePrioridad"] = indicePrioridad;
     return obj;
 }
 
 void InterfazPersona::fromJson(const Json::Value& obj) {
     nombre = obj["nombre"].asString();
+    tipoPersona = obj["tipoPersona"].asString();
+    edad = obj["edad"].asInt();
     indicePrioridad = obj["indicePrioridad"].asBool();
 }
