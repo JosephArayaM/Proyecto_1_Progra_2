b547f2c56ac85b13244fe40960b2632150cc8ce2
chrisb2004 <chrisblanco.2004@gmail.com>
2025-09-21 10:51:34 -0600

    Update Persona.cpp

--- Persona.cpp
+++ Persona.cpp
@@ -4,19 +4,23 @@
 
 #include "Persona.h"
 
-Persona::Persona(const string& nombre, bool indicePrioridad)
-    : InterfazPersona(nombre, indicePrioridad) {}
+Persona::Persona(const string& nombre, const string& tipoPersona, int edad, bool indicePrioridad)
+    : InterfazPersona(nombre, tipoPersona, edad, indicePrioridad) {}
 
 Persona::~Persona() = default;
 
 Json::Value Persona::toJson() const {
     Json::Value obj;
     obj["nombre"] = nombre;
+    obj["tipoPersona"] = tipoPersona;
+    obj["edad"] = edad;
     obj["indicePrioridad"] = indicePrioridad;
     return obj;
 }
 
 void Persona::fromJson(const Json::Value& obj) {
     nombre = obj["nombre"].asString();
+    tipoPersona = obj["tipoPersona"].asString();
+    edad = obj["edad"].asInt();
     indicePrioridad = obj["indicePrioridad"].asBool();
 }
